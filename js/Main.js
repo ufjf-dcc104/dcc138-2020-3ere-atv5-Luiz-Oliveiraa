898571f2680a02ef7e12b9a759bbe4d124ea4822
Luiz H. de Oliveira Pereira <luiz.pereira@ice.ufjf.br>
2021-02-22 13:39:55 -0300

    Cria e usa uma classe de Cena

--- js/Main.js
+++ js/Main.js
@@ -1,3 +1,6 @@
+import Cena from "./Cena.js";
 console.log("Hello world!!!");
 const canvas = document.querySelector("canvas"); //Primeiro elemento da classe canvas
 console.log(canvas);
+const cena1 = new Cena(canvas);
+cena1.desenhar();
