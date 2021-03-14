47295fb0e54dd5a66d5684a904ee660cabe7f689
Luiz H. de Oliveira Pereira <luiz.pereira@ice.ufjf.br>
2021-03-14 18:52:09 -0300

    Cria Arquivo de gerenciador de recursos

--- js/Main.js
+++ js/Main.js
@@ -1,12 +1,24 @@
 import Cena from "./Cena.js";
 import Sprite from "./Sprite.js";
-console.log("Hello world!!!");
+import AssetManager from "./AssetManager.js";
+
+    const img1 = new Image();
+    img1.src = "assets/garota.png";
+    const img2 = new Image();
+    img2.src = "assets/skelly.png";
+    const img3 = new Image();
+    img3.src = "assets/orc.png";
+    document.body.appendChild(img1);
+    document.body.appendChild(img2);
+    document.body.appendChild(img3);
+
+    const assets = new AssetManager();
 
 const canvas = document.querySelector("canvas"); //Primeiro elemento da classe canvas
 const ctx = canvas.getContext("2d");
 console.log(canvas);
 
-const cena1 = new Cena(canvas);
+const cena1 = new Cena(canvas, assets);
 const pc = new Sprite({vx:10});
 const en1 = new Sprite({x:140, w:30, color:"red"});
 
@@ -14,7 +26,7 @@ cena1.adicionar(pc);
 cena1.adicionar(en1);
 cena1.adicionar(new Sprite({y:40, w:30, color:"red"}));
 
-// cena1.iniciar();
+cena1.iniciar();
 
 document.addEventListener("keydown", (e)=>{
     switch (e.key) {
